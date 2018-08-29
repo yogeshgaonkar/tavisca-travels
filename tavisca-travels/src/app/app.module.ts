c4a6ae42298dd1cdfcb00db51d87f79ae1eb9d37
Yogesh Gaonkar <yogesh.gaonkar@corp.intelliswift.com>
2018-08-29 10:56:16 +0530

    added Angular Material

--- tavisca-travels/src/app/app.module.ts
+++ tavisca-travels/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,MatButtonModule, MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
