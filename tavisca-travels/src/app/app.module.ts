20de84e81c6c6b620604d3eda64d5f38f062ede9
Yogesh Gaonkar <yogesh.gaonkar@corp.intelliswift.com>
2018-08-29 15:55:01 +0530

    created folder structure and added routing

--- tavisca-travels/src/app/app.module.ts
+++ tavisca-travels/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { AppRoutingModule } from './/app-routing.module';
+import {PagesModule} from '../pages/pages.module'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,BrowserAnimationsModule,MatButtonModule, MatCheckboxModule
+    BrowserModule,BrowserAnimationsModule,MatButtonModule,
+     MatCheckboxModule, AppRoutingModule,PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
