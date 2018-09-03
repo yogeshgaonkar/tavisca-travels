481672ef92ad3e118c3220c383e419b006e24b18
Yogesh Gaonkar <yogesh.gaonkar@corp.intelliswift.com>
2018-09-03 17:34:54 +0530

    test commit

--- tavisca-travels/src/app/app.module.ts
+++ tavisca-travels/src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TravelMaterialModule } from './travel-material.module';
-import {PagesModule} from '../pages/pages.module'
+import {PagesModule} from '../pages/pages.module';
 
 @NgModule({
   declarations: [
