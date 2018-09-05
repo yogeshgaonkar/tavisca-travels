8bd1750cf26760394929184715ee80608f7ae536
shivintelli <39829086+shivintelli@users.noreply.github.com>
2018-09-05 14:57:47 +0530

    change country json & search changes

--- src/shared/pipes/hotel-search-filter.pipe.ts
+++ src/shared/pipes/hotel-search-filter.pipe.ts
@@ -14,7 +14,7 @@ export class HotelSeachFilterPipe implements PipeTransform {
       }
   searchText = searchText.toLowerCase();
   return items.filter( it => {
-        return it.toLowerCase().includes(searchText);
+        return it.name.toLowerCase().includes(searchText);
       });
      }
   }
