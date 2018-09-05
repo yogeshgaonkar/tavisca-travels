71d5236c5440bb9ab3860705722a7441c2cb8e44
shivintelli <39829086+shivintelli@users.noreply.github.com>
2018-09-05 18:45:51 +0530

    added guest comp

--- src/shared/components/advanced-search-component/advanced-search-component.component.ts
+++ src/shared/components/advanced-search-component/advanced-search-component.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advanced-search-component.component.scss']
 })
 export class AdvancedSearchComponentComponent implements OnInit {
-
-  constructor() { }
+  showGuestDropdown: boolean;
+  showRoomDropdown: boolean;
+  guests: Array<Object> = [
+    { title: 'Adults', total: 0, subTitle: '' },
+    { title: 'Children', total: 0, subTitle: '(Up to 14 years)' },
+    { title: 'Infants', total: 0, subTitle: '(Under 2 years)'}
+  ];
+  rooms: number;
+  constructor() {
+    this.showGuestDropdown = false;
+    this.showRoomDropdown = false;
+    this.rooms = 0;
+   }
 
   ngOnInit() { }
 
-  onSelectCity() { }
+  onSelectGuest() {
+    this.showGuestDropdown = !this.showGuestDropdown;
+  }
+
+  onSelectRoom() {
+    this.showRoomDropdown = !this.showRoomDropdown;
+  }
 }
