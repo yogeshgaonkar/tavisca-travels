999c5e6a6c7dbfc8d0e034cc13634aa7a9f638fe
Yogesh Gaonkar <yogesh.gaonkar@corp.intelliswift.com>
2018-09-10 15:53:38 +0530

    calendar added for mobile search

--- src/shared/components/mobile-advacnded-search/mobile-advacnded-search.component.ts
+++ src/shared/components/mobile-advacnded-search/mobile-advacnded-search.component.ts
@@ -9,7 +9,21 @@ import {GuestMobileComponent} from '../guest-mobile/guest-mobile.component';
 })
 export class MobileAdvacndedSearchComponent implements OnInit {
   dialogRef: any;
-  constructor(public dialog: MatDialog) { }
+  isAdvancedSearch = false;
+  bsValue = new Date();
+  bsRangeValue: Date[];
+  maxDate = new Date();
+  rangeValue: Date[];
+  checkOutDateText = 'Check Out';
+  checkInDateText = 'Check In';
+  checkInDate = new Date();
+  checkOutDate = new Date();
+  weekdayList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ];
+  monthsNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  constructor(public dialog: MatDialog) { 
+    this.maxDate.setDate(this.maxDate.getDate() + 7);
+    this.bsRangeValue = [this.bsValue, this.maxDate];
+  }
 
   ngOnInit() {
   }
@@ -23,5 +37,19 @@ export class MobileAdvacndedSearchComponent implements OnInit {
     });
   }
 
+  expanAdvancedSearch() {
+    this.isAdvancedSearch = !this.isAdvancedSearch;
+  }
+  getFormatedDate() {
+    this.checkInDate = this.rangeValue[0];
+    this.checkOutDate = this.rangeValue[1];
+    this.setDateValues();
+  }
 
+  setDateValues() {
+    this.checkOutDateText = this.weekdayList[this.checkInDate.getDay()] +
+    ' ' + this.checkInDate.getDate() + ' ' + this.monthsNames[this.checkInDate.getMonth()] + ' ' + this.checkOutDate.getFullYear();
+   this.checkInDateText = this.weekdayList[this.checkOutDate.getDay()] +
+    ' ' + this.checkOutDate.getDate() + ' ' + this.monthsNames[this.checkOutDate.getMonth()] + ' ' + this.checkOutDate.getFullYear();
+  }
 }
